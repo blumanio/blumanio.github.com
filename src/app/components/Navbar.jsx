1a10b9cb48f27ec222a1f663c06ac8e147260fcb
Mohamed El Aammari <medlique@gmail.com>
2021-12-09 21:58:31 +0100

    new animated navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,7 +1,50 @@
-import React, { Component } from "react";
+import React, {  useRef, useEffect } from "react";
+import anime from "animejs"
 
-export default class Navbar extends Component {
-  render() {
+export default function Navbar()  {
+  const textWrapper = useRef(null)
+  console.log("textWrapper",textWrapper.current);
+  useEffect(() => {
+    textWrapper.current.innerHTML = textWrapper.current.textContent.replace(
+      /([^\x00-\x80]|\w)/g,
+      "<span className ='letter'>$&</span>"
+    );
+
+    anime
+      .add({
+        targets: ".ml11 .line",
+        scaleY: [0, 1],
+        opacity: [0.5, 1],
+        easing: "easeOutExpo",
+        duration: 700,
+      })
+      .add({
+        targets: ".ml11 .line",
+        translateX: [0,textWrapper.current.getBoundingClientRect().width + 10,],
+        easing: "easeOutExpo",
+        duration: 700,
+        delay: 100,
+      })
+      .add({
+        targets: ".ml11 .letter",
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: "-=775",
+        delay: (el, i) => 34 * (i + 1),
+      })
+      .add({
+        targets: ".ml11",
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000,
+      });
+  },[textWrapper])
+  // render() {
+    // Wrap every letter in a span
+    // var textWrapper = document.querySelector(".ml11 .letters");
+    
     return (
       <div className="Navbar">
         <ul className="nav nav-tabs" id="navId">
@@ -26,6 +69,16 @@ export default class Navbar extends Component {
               Disabled
             </a>
           </li>
+          <li>
+            <h1 ref = {textWrapper} className ="ml11">
+              <span className ="text-wrapper">
+                <span className ="line line1"></span>
+                <span ref = {textWrapper} className ="letters">Hello Goodbye</span>
+              </span>
+            </h1>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+          </li>
         </ul>
 
         <div className="tab-content">
@@ -41,5 +94,5 @@ export default class Navbar extends Component {
         </div>
       </div>
     );
-  }
+  // }
 }
